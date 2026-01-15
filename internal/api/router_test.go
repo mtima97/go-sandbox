c9e7aa4162de0f4b4c40419d492d88e479681401
mtima97 <mtima97@gmail.com>
2026-01-15 18:32:50 +0000

    RF: improved the test

--- internal/api/router_test.go
+++ internal/api/router_test.go
@@ -13,26 +13,38 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type FakeDb struct {
-	//
-}
+type FakeDb struct{}
 
 func (r FakeDb) GetEntityByName(ctx context.Context, name, language string) (json.RawMessage, error) {
 	return json.RawMessage("[]"), nil
 }
 
-func setupRouter() *gin.Engine {
+func setupRouter(c func(k, v string)) *gin.Engine {
 	h := handlers.NewCvHandler(service.NewCvService(FakeDb{}))
 
-	c := config.Cfg{
-		AllowedOrigins: []string{"*"},
+	envMock := map[string]string{
+		"APP_PORT":        "8080",
+		"ALLOWED_ORIGINS": "*",
+		"DB_USER":         "",
+		"DB_PASS":         "",
+		"DB_HOST":         "",
+		"DB_PORT":         "",
+		"DB_NAME":         "",
 	}
 
-	return Register(h, c)
+	for k, v := range envMock {
+		c(k, v)
+	}
+
+	conf, _ := config.Load()
+
+	return Register(h, conf)
 }
 
 func TestTheEndpoint(t *testing.T) {
-	r := setupRouter()
+	r := setupRouter(func(k, v string) {
+		t.Setenv(k, v)
+	})
 
 	rec := httptest.NewRecorder()
 	req := httptest.NewRequest(http.MethodGet, "/api/v2/cv?lang=ru", nil)
